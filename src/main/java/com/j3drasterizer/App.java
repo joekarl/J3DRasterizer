011b2294690b6eca746ecaafebc636fcb967eab3
Karl Kirch <karl.ctr.kirch@faa.gov>
2012-10-09 12:13:27 -0500

    Initial commit

---
+++ src/main/java/com/j3drasterizer/App.java
@@ -0,0 +1,13 @@
+package com.j3drasterizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
